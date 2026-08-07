98c929f776999814cfaadad6879daba1bec80588
wissalto485-lgtm <wissalto485@gmail.com>
2026-08-07 17:15:16 +0100

    https://radjaaaccessoire.onrender.com

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ app.use(helmet({
 const corsOptions = {
     origin: function (origin, callback) {
         const allowedOrigins = [
+            'https://radjaaaccessoire.onrender.com',
             'http://localhost:5500',
             'http://127.0.0.1:5500',
             'http://localhost:5000',
